2cf1f3347a7ec447e3c726be56cc2c57469d15e2
andy-ngn <andy@ariadnemaps.com>
2023-10-08 23:11:06 +0200

    installed lib

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,78 +1,13 @@
 import { Inter } from "next/font/google";
-import { useEffect, useRef, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [enabled, setEnabled] = useState(false);
-  const [bearing, setBearing] = useState(0);
-  const [alpha, setAlpha] = useState(0);
-  const [beta, setBeta] = useState(0);
-  const [gamma, setGamma] = useState(0);
-
-  const askPermissionRef = useRef<boolean>(false);
-  const askPermission = askPermissionRef.current;
-  useEffect(() => {
-    function handleOrientation(event: any) {
-      requestAnimationFrame(() => {
-        setBearing(event.webkitCompassHeading);
-        setAlpha(event.webkitCompassHeading);
-        setBeta(event.beta);
-        setGamma(event.gamma);
-      });
-    }
-    function handleOrientationAndroid(event: any) {
-      requestAnimationFrame(() => {
-        if (
-          !event.absolute ||
-          event.alpha == null ||
-          event.beta == null ||
-          event.gamma == null
-        ) {
-          let compass = -(event.alpha + (event.beta * event.gamma) / 90);
-          compass -= Math.floor(compass / 360) * 360; // Wrap into range [0,360]
-
-          setBearing(compass);
-          setAlpha(compass);
-          setBeta(event.beta);
-          setGamma(event.gamma);
-        }
-      });
-    }
-    function handleMotion(event: any) {
-      // requestAnimationFrame(() => {
-      //   setDax(event.accelerationIncludingGravity.x);
-      //   setDay(event.accelerationIncludingGravity.y);
-      //   setDaz(event.accelerationIncludingGravity.z);
-      //   setCalcFlag(true);
-      //   setCheckStep(true);
-      // });
-    }
-    if (askPermission) {
-      askPermissionRef.current = false;
-      {
-        window.addEventListener(
-          "deviceorientationabsolute",
-          handleOrientationAndroid,
-          true
-        );
-        window.addEventListener("devicemotion", handleMotion, true);
-      }
-    }
-  }, [askPermission]);
-
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <button
-        onClick={() => {
-          askPermissionRef.current = true;
-        }}
-      >
-        Start
-      </button>
-      {JSON.stringify({ bearing, alpha, beta, gamma }, null, 2)}
+      Helo
     </main>
   );
 }
---
+++ src/pages/test.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import {
+  AbsoluteOrientationSensor,
+  Accelerometer,
+  GravitySensor,
+  Gyroscope,
+  LinearAccelerationSensor,
+  OrientationSensor,
+  RelativeOrientationSensor,
+  Sensor,
+} from "motion-sensors-polyfill";
+const Page = () => {
+  const [data, setData] = useState<any>({});
+  const [error, setError] = useState<any>();
+  useEffect(() => {
+    if ("Accelerometer" in window) {
+      let accelerometer = null;
+      try {
+        accelerometer = new Accelerometer({ frequency: 10 });
+        accelerometer.onerror = (event: any) => {
+          // Handle runtime errors.
+          if (event.error.name === "NotAllowedError") {
+            console.log("Permission to access sensor was denied.");
+          } else if (event.error.name === "NotReadableError") {
+            console.log("Cannot connect to the sensor.");
+          }
+        };
+        accelerometer.onreading = (e) => {
+          setData(e);
+        };
+        accelerometer.start();
+      } catch (error: any) {
+        // Handle construction errors.
+        if (error.name === "SecurityError") {
+          console.log(
+            "Sensor construction was blocked by the Permissions Policy."
+          );
+        } else if (error.name === "ReferenceError") {
+          console.log("Sensor is not supported by the User Agent.");
+        } else {
+          setError(error);
+        }
+      }
+    }
+  }, []);
+  return (
+    <div>
+      Page
+      <div>{!data ? "Nothing" : JSON.stringify(data)}</div>
+    </div>
+  );
+};
+
+export default Page;
