a24818ce962cf1e1517f5a979cbb79c5f835e694
andy-ngn <andy@ariadnemaps.com>
2023-10-08 23:23:39 +0200

    again

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,36 +1,38 @@
 import React, { useEffect, useState } from "react";
-
+import type { Accelerometer } from "motion-sensors-polyfill";
 const Page = () => {
-  const [data, setData] = useState<any>({});
+  const [data, setData] = useState<any>(null);
   const [error, setError] = useState<any>();
   useEffect(() => {
-    if ("Accelerometer" in window) {
-      let accelerometer = null;
-      try {
-        accelerometer = new Accelerometer({ frequency: 10 });
-        accelerometer.onerror = (event: any) => {
-          // Handle runtime errors.
-          if (event.error.name === "NotAllowedError") {
-            console.log("Permission to access sensor was denied.");
-          } else if (event.error.name === "NotReadableError") {
-            console.log("Cannot connect to the sensor.");
-          }
-        };
-        accelerometer.onreading = (e) => {
-          setData(e);
-        };
-        accelerometer.start();
-      } catch (error: any) {
-        // Handle construction errors.
-        if (error.name === "SecurityError") {
-          console.log(
-            "Sensor construction was blocked by the Permissions Policy."
-          );
-        } else if (error.name === "ReferenceError") {
-          console.log("Sensor is not supported by the User Agent.");
-        } else {
-          setError(error);
+    let accelerometer: Accelerometer | null = null;
+    try {
+      //@ts-expect-error
+      accelerometer = new Accelerometer({ referenceFrame: "device" });
+      accelerometer.addEventListener("error", (event) => {
+        // Handle runtime errors.
+        //@ts-expect-error
+        if (event.error.name === "NotAllowedError") {
+          // Branch to code for requesting permission.
+          //@ts-expect-error
+        } else if (event.error.name === "NotReadableError") {
+          console.log("Cannot connect to the sensor.");
         }
+      });
+      accelerometer.addEventListener("reading", () => {
+        reloadOnShake(accelerometer);
+        if (!accelerometer) return;
+        setData({ x: accelerometer.x, y: accelerometer.y, z: accelerometer.z });
+      });
+      accelerometer.start();
+    } catch (error) {
+      // Handle construction errors.
+      if (error.name === "SecurityError") {
+        // See the note above about permissions policy.
+        console.log("Sensor construction was blocked by a permissions policy.");
+      } else if (error.name === "ReferenceError") {
+        console.log("Sensor is not supported by the User Agent.");
+      } else {
+        throw error;
       }
     }
   }, []);
@@ -38,6 +40,7 @@ const Page = () => {
     <div>
       Page
       <div>{!data ? "Nothing" : JSON.stringify(data)}</div>
+      {error && <div>Error: {JSON.stringify(error)}</div>}
     </div>
   );
 };
