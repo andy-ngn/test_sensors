479d05cf2295e5b77a964579f0f1086baf74209d
andy-ngn <andy@ariadnemaps.com>
2023-10-22 11:34:47 +0200

    test 2

--- src/components/bases/Map.tsx
+++ src/components/bases/Map.tsx
@@ -1,3 +1,4 @@
+import "mapbox-gl/dist/mapbox-gl.css";
 import React from "react";
 import Map from "react-map-gl";
 import mapbox from "mapbox-gl";
@@ -5,24 +6,31 @@ const BaseMap = () => {
   if (!mapbox.supported())
     return <div className='container'>Sorry GL is not supported</div>;
   return (
-    <Map
+    <div
       style={{
-        position: "absolute",
-        width: "100vw",
+        width: "100%",
+        position: "relative",
         height: "100vh",
-        top: 0,
-        left: 0,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
       }}
-      initialViewState={{
-        bearing: 0,
-        zoom: 19,
-        latitude: 45.86575,
-        longitude: 13.43268,
-        pitch: 0,
-      }}
-      mapboxAccessToken='pk.eyJ1IjoiaWI5OCIsImEiOiJjbGhhbTRuaXUwOGliM2Ruc3h2YTFoMG9yIn0.YtRfHX0vI6aXB6WBD6hajg'
-      mapStyle={`mapbox://styles/mapbox/light-v11`}
-    ></Map>
+    >
+      <div>Something here</div>
+      <Map
+        style={{ height: "70%" }}
+        initialViewState={{
+          bearing: 0,
+          zoom: 19,
+          latitude: 45.86575,
+          longitude: 13.43268,
+          pitch: 0,
+        }}
+        mapboxAccessToken='pk.eyJ1IjoiaWI5OCIsImEiOiJjbGhhbTRuaXUwOGliM2Ruc3h2YTFoMG9yIn0.YtRfHX0vI6aXB6WBD6hajg'
+        mapStyle={`mapbox://styles/mapbox/light-v11`}
+      ></Map>
+    </div>
   );
 };
 
