3bb855e7eafca415a8564fa137709400cb547ea5
andy-ngn <andy@ariadnemaps.com>
2023-10-22 12:08:41 +0200

    attempt

--- src/components/bases/LeafletMap.tsx
+++ src/components/bases/LeafletMap.tsx
@@ -4,23 +4,23 @@ import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 const LeafletMap = () => {
   return (
-    <MapContainer
-      style={{
-        position: "absolute",
-        top: 0,
-        left: 0,
-        height: "50vh",
-        width: "100vw",
-      }}
-      center={[51.505, -0.09]}
-      zoom={13}
-      scrollWheelZoom={false}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-      />
-    </MapContainer>
+    <div className='flex relative justify-center items-center w-full border h-screen flex-col'>
+      sas
+      <MapContainer
+        style={{
+          height: "30%",
+          width: "100%",
+        }}
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+      </MapContainer>
+    </div>
   );
 };
 
