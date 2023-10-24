da6c6cf4006fa1346a95440b15c87fde5bfb20eb
andy-ngn <andy@ariadnemaps.com>
2023-10-24 14:04:40 +0200

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,102 @@
 import map from "mapbox-gl";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 export default function Home() {
-  const [status, setStatus] = useState("Default");
-  useEffect(() => {
-    if (!map.supported()) {
-      setStatus("GL Not Supported");
-    } else {
-      setStatus("Running");
-    }
+  const [ask, setAsk] = useState(false);
+  const [data, setData] = useState<{ d1: Object; d2: Object; d3: Object }>({
+    d1: {},
+    d2: {},
+    d3: {},
+  });
+
+  const handleOrientation = useCallback((event: any) => {
+    requestAnimationFrame(() => {
+      setData((prev) => ({ ...prev, d1: event }));
+    });
   }, []);
+  const handleMotion = useCallback((event: any) => {
+    requestAnimationFrame(() => {
+      setData((prev) => ({ ...prev, d3: event }));
+    });
+  }, []);
+  const handleOrientationAndroid = useCallback((event: any) => {
+    requestAnimationFrame(() => {
+      if (
+        !event.absolute ||
+        event.alpha == null ||
+        event.beta == null ||
+        event.gamma == null
+      ) {
+        setData((prev) => ({ ...prev, d2: event }));
+      }
+    });
+  }, []);
+  useEffect(() => {
+    if (ask) {
+      setAsk(false);
+      const tmpIos =
+        navigator.userAgent.match(/(iPod|iPhone|iPad)/) &&
+        navigator.userAgent.match(/AppleWebKit/);
+      if (tmpIos) {
+        let hasRequestPermission = false;
+        try {
+          hasRequestPermission =
+            typeof DeviceOrientationEvent !== "undefined" &&
+            //@ts-expect-error
+            typeof DeviceOrientationEvent.requestPermission === "function" &&
+            typeof DeviceMotionEvent !== "undefined" &&
+            //@ts-expect-error
+            typeof DeviceMotionEvent.requestPermission === "function";
+        } catch (error) {
+          hasRequestPermission = false;
+        }
+
+        if (!hasRequestPermission) return;
+        //@ts-expect-error
+        DeviceOrientationEvent.requestPermission()
+          .then((response: any) => {
+            if (response === "granted") {
+              window.addEventListener(
+                "deviceorientation",
+                handleOrientation,
+                true
+              );
+            } else {
+              alert("has to be allowed!");
+            }
+          })
+          .catch((err: any) => alert(err));
+        //@ts-expect-error
+        DeviceMotionEvent.requestPermission()
+          .then((response: any) => {
+            if (response === "granted") {
+              window.addEventListener("devicemotion", handleMotion, true);
+            } else {
+              alert("has to be allowed! motion");
+            }
+          })
+          .catch((err: any) => alert(err));
+      } else {
+        window.addEventListener(
+          "deviceorientationabsolute",
+          handleOrientationAndroid,
+          true
+        );
+        window.addEventListener("devicemotion", handleMotion, true);
+      }
+    }
+  }, [handleMotion, handleOrientation, handleOrientationAndroid]);
   return (
     <main>
       <h1>Home</h1>
-      <div>Status: {status}</div>
+      <pre>Data :{JSON.stringify(data, null, 2)}</pre>
+
+      <div>
+        <button
+          onClick={() => {
+            setAsk(true);
+          }}
+        ></button>
+      </div>
     </main>
   );
 }
