6fdd92e8a6c829e2d1f561cdcd09e5a49e2bef7e
andy-ngn <andy@ariadnemaps.com>
2023-10-24 14:18:19 +0200

    sadas

---
+++ src/components/Sensors.tsx
@@ -0,0 +1,143 @@
+//@ts-nocheck
+
+import { useEffect, useMemo, useState } from "react";
+
+import Waveform from "@/components/waveform";
+import EazIndicator from "@/components/EazIndicator";
+
+type Props = {
+  viewState: object;
+  setViewState: Function;
+  askPermission: boolean;
+  setAskPermission: Function;
+};
+
+const Sensors = ({
+  viewState,
+  setViewState,
+  askPermission,
+  setAskPermission,
+}: Props) => {
+  const [Bearing, setBearing] = useState(viewState?.bearing);
+  const [checkStep, setCheckStep] = useState(false);
+
+  const [alpha, setAlpha] = useState(0);
+  const [beta, setBeta] = useState(0);
+  const [gamma, setGamma] = useState(0);
+  const [dax, setDax] = useState(0);
+  const [day, setDay] = useState(0);
+  const [daz, setDaz] = useState(0);
+
+  const [eazentry, setEazentry] = useState(null);
+
+  const [timestampOfLastAriadnePos, setTimestampOfLastAriadnePos] = useState(0);
+
+  const [isIos, setIsIos] = useState("undefined");
+
+  useEffect(() => {
+    function handleOrientation(event) {
+      requestAnimationFrame(() => {
+        setViewState({ ...viewState, bearing: event.webkitCompassHeading });
+        setBearing(event.webkitCompassHeading);
+        setAlpha(event.webkitCompassHeading);
+        setBeta(event.beta);
+        setGamma(event.gamma);
+        // log(`${event.webkitCompassHeading} , ${Math.abs(event.alpha - 360)}`)
+      });
+    }
+    function handleOrientationAndorid(event) {
+      requestAnimationFrame(() => {
+        if (
+          !event.absolute ||
+          event.alpha == null ||
+          event.beta == null ||
+          event.gamma == null
+        )
+          log("running in pc?");
+        let compass = -(event.alpha + (event.beta * event.gamma) / 90);
+        compass -= Math.floor(compass / 360) * 360; // Wrap into range [0,360]
+        setViewState({ ...viewState, bearing: compass });
+        setBearing(compass);
+        setAlpha(compass);
+        setBeta(event.beta);
+        setGamma(event.gamma);
+        // log(`${event.webkitCompassHeading} , ${Math.abs(event.alpha - 360)}`)
+      });
+    }
+    function handleMotion(event) {
+      requestAnimationFrame(() => {
+        setDax(event.accelerationIncludingGravity.x);
+        setDay(event.accelerationIncludingGravity.y);
+        setDaz(event.accelerationIncludingGravity.z);
+        setCalcFlag(true);
+        setCheckStep(true);
+      });
+    }
+    if (isIos == "undefined" && askPermission) {
+      setAskPermission(false);
+      const tmpIos =
+        navigator.userAgent.match(/(iPod|iPhone|iPad)/) &&
+        navigator.userAgent.match(/AppleWebKit/);
+      setIsIos(tmpIos);
+
+      if (tmpIos) {
+        let hasRequestPermission = false;
+        try {
+          hasRequestPermission =
+            typeof DeviceOrientationEvent !== "undefined" &&
+            typeof DeviceOrientationEvent.requestPermission === "function" &&
+            typeof DeviceMotionEvent !== "undefined" &&
+            typeof DeviceMotionEvent.requestPermission === "function";
+        } catch (error) {
+          hasRequestPermission = false;
+        }
+
+        if (!hasRequestPermission) return;
+        DeviceOrientationEvent.requestPermission()
+          .then((response) => {
+            if (response === "granted") {
+              log("success");
+              window.addEventListener(
+                "deviceorientation",
+                handleOrientation,
+                true
+              );
+            } else {
+              alert("has to be allowed!");
+            }
+          })
+          .catch((err) => alert(err));
+
+        DeviceMotionEvent.requestPermission()
+          .then((response) => {
+            if (response === "granted") {
+              log("success motion");
+              window.addEventListener("devicemotion", handleMotion, true);
+            } else {
+              alert("has to be allowed! motion");
+            }
+          })
+          .catch((err) => alert(err));
+      } else {
+        window.addEventListener(
+          "deviceorientationabsolute",
+          handleOrientationAndorid,
+          true
+        );
+        window.addEventListener("devicemotion", handleMotion, true);
+      }
+    }
+    // }
+  }, [askPermission, isIos]);
+
+  /////////////////////////////////////////////////////////////////////////INCREAS WEIGHTS CLOSE TO PATH LINE STRING
+
+  return (
+    <>
+      <Waveform eaz={eazentry} setVariance={setVariance}></Waveform>
+      <EazIndicator eaz={timestampOfLastAriadnePos} />
+    </>
+  );
+};
+
+export default Sensors;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,95 +1,96 @@
+import Sensors from "@/components/Sensors";
 import { Button } from "@/components/ui/button";
-import map from "mapbox-gl";
-import { useCallback, useEffect, useState } from "react";
+
+import { useState } from "react";
 export default function Home() {
   const [ask, setAsk] = useState(false);
-  const [data, setData] = useState<{ d1: Object; d2: Object; d3: Object }>({
-    d1: {},
-    d2: {},
-    d3: {},
-  });
+  const [viewState, setViewState] = useState<Object>({});
+  // const [data, setData] = useState<{ d1: Object; d2: Object; d3: Object }>({
+  //   d1: {},
+  //   d2: {},
+  //   d3: {},
+  // });
 
-  const handleOrientation = useCallback((event: any) => {
-    requestAnimationFrame(() => {
-      setData((prev) => ({ ...prev, d1: event }));
-    });
-  }, []);
-  const handleMotion = useCallback((event: any) => {
-    requestAnimationFrame(() => {
-      setData((prev) => ({ ...prev, d3: event }));
-    });
-  }, []);
-  const handleOrientationAndroid = useCallback((event: any) => {
-    requestAnimationFrame(() => {
-      if (
-        !event.absolute ||
-        event.alpha == null ||
-        event.beta == null ||
-        event.gamma == null
-      ) {
-        setData((prev) => ({ ...prev, d2: event }));
-      }
-    });
-  }, []);
-  useEffect(() => {
-    if (ask) {
-      setAsk(false);
-      const tmpIos =
-        navigator.userAgent.match(/(iPod|iPhone|iPad)/) &&
-        navigator.userAgent.match(/AppleWebKit/);
-      if (tmpIos) {
-        let hasRequestPermission = false;
-        try {
-          hasRequestPermission =
-            typeof DeviceOrientationEvent !== "undefined" &&
-            //@ts-expect-error
-            typeof DeviceOrientationEvent.requestPermission === "function" &&
-            typeof DeviceMotionEvent !== "undefined" &&
-            //@ts-expect-error
-            typeof DeviceMotionEvent.requestPermission === "function";
-        } catch (error) {
-          hasRequestPermission = false;
-        }
+  // const handleOrientation = useCallback((event: any) => {
+  //   requestAnimationFrame(() => {
+  //     setData((prev) => ({ ...prev, d1: event }));
+  //   });
+  // }, []);
+  // const handleMotion = useCallback((event: any) => {
+  //   requestAnimationFrame(() => {
+  //     setData((prev) => ({ ...prev, d3: event }));
+  //   });
+  // }, []);
+  // const handleOrientationAndroid = useCallback((event: any) => {
+  //   requestAnimationFrame(() => {
+  //     if (
+  //       !event.absolute ||
+  //       event.alpha == null ||
+  //       event.beta == null ||
+  //       event.gamma == null
+  //     ) {
+  //       setData((prev) => ({ ...prev, d2: event }));
+  //     }
+  //   });
+  // }, []);
+  // useEffect(() => {
+  //   if (ask) {
+  //     setAsk(false);
+  //     const tmpIos =
+  //       navigator.userAgent.match(/(iPod|iPhone|iPad)/) &&
+  //       navigator.userAgent.match(/AppleWebKit/);
+  //     if (tmpIos) {
+  //       let hasRequestPermission = false;
+  //       try {
+  //         hasRequestPermission =
+  //           typeof DeviceOrientationEvent !== "undefined" &&
+  //           //@ts-expect-error
+  //           typeof DeviceOrientationEvent.requestPermission === "function" &&
+  //           typeof DeviceMotionEvent !== "undefined" &&
+  //           //@ts-expect-error
+  //           typeof DeviceMotionEvent.requestPermission === "function";
+  //       } catch (error) {
+  //         hasRequestPermission = false;
+  //       }
 
-        if (!hasRequestPermission) return;
-        //@ts-expect-error
-        DeviceOrientationEvent.requestPermission()
-          .then((response: any) => {
-            if (response === "granted") {
-              window.addEventListener(
-                "deviceorientation",
-                handleOrientation,
-                true
-              );
-            } else {
-              alert("has to be allowed!");
-            }
-          })
-          .catch((err: any) => alert(err));
-        //@ts-expect-error
-        DeviceMotionEvent.requestPermission()
-          .then((response: any) => {
-            if (response === "granted") {
-              window.addEventListener("devicemotion", handleMotion, true);
-            } else {
-              alert("has to be allowed! motion");
-            }
-          })
-          .catch((err: any) => alert(err));
-      } else {
-        window.addEventListener(
-          "deviceorientationabsolute",
-          handleOrientationAndroid,
-          true
-        );
-        window.addEventListener("devicemotion", handleMotion, true);
-      }
-    }
-  }, [handleMotion, handleOrientation, handleOrientationAndroid]);
+  //       if (!hasRequestPermission) return;
+  //       //@ts-expect-error
+  //       DeviceOrientationEvent.requestPermission()
+  //         .then((response: any) => {
+  //           if (response === "granted") {
+  //             window.addEventListener(
+  //               "deviceorientation",
+  //               handleOrientation,
+  //               true
+  //             );
+  //           } else {
+  //             alert("has to be allowed!");
+  //           }
+  //         })
+  //         .catch((err: any) => alert(err));
+  //       //@ts-expect-error
+  //       DeviceMotionEvent.requestPermission()
+  //         .then((response: any) => {
+  //           if (response === "granted") {
+  //             window.addEventListener("devicemotion", handleMotion, true);
+  //           } else {
+  //             alert("has to be allowed! motion");
+  //           }
+  //         })
+  //         .catch((err: any) => alert(err));
+  //     } else {
+  //       window.addEventListener(
+  //         "deviceorientationabsolute",
+  //         handleOrientationAndroid,
+  //         true
+  //       );
+  //       window.addEventListener("devicemotion", handleMotion, true);
+  //     }
+  //   }
+  // }, [handleMotion, handleOrientation, handleOrientationAndroid]);
   return (
     <main>
       <h1>Home</h1>
-      <pre>Data :{JSON.stringify(data, null, 2)}</pre>
 
       <div>
         <Button
@@ -100,6 +101,15 @@ export default function Home() {
           Start
         </Button>
       </div>
+      <div>
+        <Sensors
+          askPermission={ask}
+          setAskPermission={setAsk}
+          setViewState={setViewState}
+          viewState={viewState}
+        />
+      </div>
+      <div>{!viewState ? null : JSON.stringify(viewState, null, 2)}</div>
     </main>
   );
 }
