fbf5bc147fa0cb7ea18d59359a011ee66a610c09
andy-ngn <andy@ariadnemaps.com>
2023-11-22 18:54:20 +0100

    twilio

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -1,77 +1,18 @@
 import type { NextApiHandler } from "next";
-import axios from "axios";
-const token =
-  "EAASemvjYD9cBOxXJYpt9ISxGSAbphTJjZBUzWz4KIBgNgxv1zfdG86ZAgwhBvx2FT12SYq6RmALs07vYZALJD2ZA1l47llTa6QqmTprh8h6ZCv93UqjtlnDstSpOQua6mVgeFNSrkXNljyjpOkMkTRTsE7FIZB9FNHejw6kGmsFVDZAeO8xBDEiVoqid2fWMFwljyszTxqOyLeZCvnZCwaoDN";
+import Twilio from "twilio";
+const accountSid = "AC3bc0e6db5c17dd06022752437a3c9178";
+const authToken = "59e46fbe8bf49bb205594a420555422b";
 const handler: NextApiHandler = async (req, res) => {
   try {
+    const client = Twilio(accountSid, authToken);
     if (req.method === "POST") {
-      let body = req.body;
-      console.log(JSON.stringify(req.body, null, 2));
-
-      if (req.body) {
-        if (
-          req.body.entry &&
-          req.body.entry[0].changes &&
-          req.body.entry[0].changes[0] &&
-          req.body.entry[0].changes[0].value.messages &&
-          req.body.entry[0].changes[0].value.messages[0]
-        ) {
-          let phone_number_id =
-            req.body.entry[0].changes[0].value.metadata.phone_number_id;
-          let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
-          let msg_body =
-            req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
-          // axios({
-          //   method: "POST", // Required, HTTP method, a string, e.g. POST, GET
-          //   url:
-          //     "https://graph.facebook.com/v12.0/" +
-          //     phone_number_id +
-          //     "/messages?access_token=" +
-          //     token,
-          //   data: {
-          //     messaging_product: "whatsapp",
-          //     to: from,
-          //     text: { body: "Ack: " + msg_body },
-          //   },
-          //   headers: { "Content-Type": "application/json" },
-          // });
-          const results = await axios.post(
-            `https://graph.facebook.com/v17.0/117329494728345/messages`,
-            {
-              messaging_product: "whatsapp",
-              to: "4917641317141",
-              type: "template",
-              template: { name: "ariadne_nav", language: { code: "en_US" } },
-            },
-            {
-              headers: {
-                Authorization: "Bearer " + token,
-                "Content-Type": "application/json",
-              },
-            }
-          );
-          console.log(results.data);
-          return res.status(200).json(results.data);
-        }
-      } else {
-        // Return a '404 Not Found' if event is not from a WhatsApp API
-        throw { status: 404, message: "No" };
-      }
+      const content = req.body;
+      await client.messages.create({
+        body: "Body: " + JSON.stringify(content, null, 2),
+        from: "whatsapp:+14155238886",
+        to: "whatsapp:+4917641317141",
+      });
     } else if (req.method === "GET") {
-      const verify_token = "some_token";
-      let mode = req.query["hub.mode"];
-      let token = req.query["hub.verify_token"];
-      let challenge = req.query["hub.challenge"];
-      if (mode && token) {
-        // Check the mode and token sent are correct
-        if (mode === "subscribe" && token === verify_token) {
-          // Respond with 200 OK and challenge token from the request
-          console.log("WEBHOOK_VERIFIED");
-          res.status(200).send(challenge);
-        } else {
-          throw { status: 403, message: "some error" };
-        }
-      }
     } else throw { status: 403, message: "Wrong method" };
   } catch (error) {
     console.log(error.message);
@@ -80,43 +21,3 @@ const handler: NextApiHandler = async (req, res) => {
 };
 
 export default handler;
-
-const bodytype = {
-  object: "whatsapp_business_account",
-  entry: [
-    {
-      id: "112258665239838",
-      changes: [
-        {
-          value: {
-            messaging_product: "whatsapp",
-            metadata: {
-              display_phone_number: "15550517359",
-              phone_number_id: "117329494728345",
-            },
-            contacts: [
-              {
-                profile: {
-                  name: "Andyy",
-                },
-                wa_id: "4917641317141",
-              },
-            ],
-            messages: [
-              {
-                from: "4917641317141",
-                id: "wamid.HBgNNDkxNzY0MTMxNzE0MRUCABIYIEJDNjk1MDlEQTMxODBEMkNCRjIyMjBFQkE4NUY1MTkzAA==",
-                timestamp: "1700566165",
-                text: {
-                  body: "Gbc",
-                },
-                type: "text",
-              },
-            ],
-          },
-          field: "messages",
-        },
-      ],
-    },
-  ],
-};
